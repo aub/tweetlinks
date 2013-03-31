c4666bb287bc2ad9590abdcbc8bb5ee32824633d
Aubrey Holland <aubrey@brewster.com>
2013-03-31 08:02:28 -0400

    initial commit, y'all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetlinks::Application.config.secret_token = 'c2c561d9bb5ce03925ddebbf8a955d76db69838fcfbdc5dc398f7207ea9f09b991516b68c971ed0f277904435ce549bb27b8302ed838649201e50b4c950f5f89'
