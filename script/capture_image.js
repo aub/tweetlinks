d7613d2f1b7ecaa287e3210694023c6a2ed35871
Aubrey Holland <aubrey@brewster.com>
2013-04-01 23:22:22 -0400

    render smaller images

--- script/capture_image.js
+++ script/capture_image.js
@@ -8,7 +8,9 @@ if (system.args.length != 3) {
 } else {
   address = system.args[1];
   output = system.args[2];
-  page.viewportSize = { width: 800, height: 1000 };
+  page.viewportSize = { width: 600, height: 600 };
+  page.clipRect = { top: 0, left: 0, width: 600, height: 600 }
+  page.zoomFactor = 0.5;
   page.open(address, function (status) {
     if (status !== 'success') {
       console.log('Unable to load the address!');
