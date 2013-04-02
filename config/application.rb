b2e9e601aaa809036c00b7603a295d8da1372e4a
Aubrey Holland <aubrey@brewster.com>
2013-04-02 16:28:46 -0400

    don't initialize on asset precompile

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Tweetlinks
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Make heroku happy during precompile
+    config.assets.initialize_on_precompile = false
   end
 end
